f143369a4469ee59e9ef2867d60d29fb7bcd1be2
Tellez6998 <tellez.03270910@gmail.com>
2023-01-15 17:02:08 -0600

    01-tipos-basicos

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,14 @@
+
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre:string = 'Lalisa';
+let hp: number | string = 27;
+let estaVivo: boolean = false
+
+console.log(nombre);
+console.log(hp);
+console.log(estaVivo);
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 
 
-console.log('Hola Mundo!');
-
 /*
     ===== Código de TypeScript =====
 */
