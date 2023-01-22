5a9e7e1a8cc7c795b42379cb56c737c2e6ed0bf4
Tellez6998 <tellez.03270910@gmail.com>
2023-01-22 16:45:45 -0600

    09-genericos

---
+++ src/ejercicios/09-genericos.ts
@@ -0,0 +1,14 @@
+
+
+/*
+    ===== Código de TypeScript =====
+*/
+function queTipoSoy<T>(argumento: T){
+    return argumento;
+}
+
+let soyString = queTipoSoy('Hola Mundo');
+let soyNumero = queTipoSoy(100);
+let soyArreglo = queTipoSoy([1,2,3,4,5,6,7,8,9,10]);
+
+let soyExplisito = queTipoSoy<number>(100);
