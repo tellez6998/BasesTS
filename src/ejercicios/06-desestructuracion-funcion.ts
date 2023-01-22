c14b0f780594c373f97651b685f719bbad684519
Tellez6998 <tellez.03270910@gmail.com>
2023-01-22 16:17:26 -0600

    07-import-export

--- src/ejercicios/06-desestructuracion-funcion.ts
+++ src/ejercicios/06-desestructuracion-funcion.ts
@@ -3,7 +3,7 @@
 /*
     ===== Código de TypeScript =====
 */
-interface Producto {
+export interface Producto {
     desc: string;
     precio: number;
 }
@@ -17,7 +17,7 @@ const tableta: Producto = {
     precio: 350
 }
 
-function calculaISV(productos: Producto[]):[number, number] {
+export function calculaISV(productos: Producto[]):[number, number] {
     let total = 0;
     productos.forEach( ({precio}) => {
         total += precio;
